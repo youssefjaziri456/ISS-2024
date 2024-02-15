9430cbae76c8a0da4336b5a3dfc686338e0f7e0a
youssef jaziri <youssefjaziri456@gmail.com>
2024-02-15 19:16:45 +0100

    Create Login Signup scripts

---
+++ App.js
@@ -0,0 +1,39 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import Login from './components/Authentification/Login';
+import Signup from './components/Authentification/Signup';
+import Welcome from './components/Welcome';
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Welcome'
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

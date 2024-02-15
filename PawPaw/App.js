f6b9b9ef8305c37a6fda0630bbbc7beed4736e29
Chaima-benyedder <chaima.benyedder@medtech.tn>
2024-02-15 17:37:55 +0100

    Implementation of the welcome page

---
+++ PawPaw/App.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import Login from "./ components/Login";
+//import Signup from "./ components/Signup";
+import Welcome from "./components/Welcome";
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ PawPaw/components/Colors.js
@@ -0,0 +1,8 @@
+const COLORS = {
+    white: "#FFFFFF",
+    black: "#222222",
+    primary: "#FDDDDD",
+    grey: "#CCCCCC"
+}
+
+export default COLORS;

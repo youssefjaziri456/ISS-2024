871864fdd1a5246db41d9da5868d6743644567a0
namnoum <97735329+namnoum@users.noreply.github.com>
2024-02-27 11:08:07 +0100

    merge with login signup

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
+//nemnom
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import TabNavigations from './App/Navigations/TabNavigations';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+//youssef
+import Login from './components/Authentification/Login';
+import Signup from './components/Authentification/Signup';
+import Welcome from './components/Welcome';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -13,6 +21,34 @@ export default function App() {
           {/* This is an empty View to reserve space for the bottom tab bar */}
         </View>
       </NavigationContainer>
+      
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Welcome'
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -28,8 +64,3 @@ const styles = StyleSheet.create({
     height: 2 // Adjust height as needed for your tab bar
   },
 });
-
-
-
-
-
